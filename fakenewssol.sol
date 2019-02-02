d19bdb3eb05ec71112dc585769a3d433a7ab6c01
Shivashish Jaiswal <shivashish987@gmail.com>
2019-02-02 19:50:52 +0530

    second commit

---
+++ fakenewssol.sol
@@ -0,0 +1,57 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Fakenews{
+    uint amount;
+    
+    struct votes{
+        uint8 notfake;
+        uint8 fake;
+        bool validate;
+    }
+    
+    mapping(uint8 => votes) votestatus;
+    mapping(address => uint) voteflag;
+    mapping(address => uint) voteCount;
+    
+
+    function postQuery(uint8 text) public{
+        require(votestatus[text].validate == false);
+        votestatus[text].notfake=0;
+        votestatus[text].fake=0;
+        votestatus[text].validate = true;
+    }
+    
+    function voteFake(uint8 text) public{
+        require(voteflag[msg.sender] != text);
+        votestatus[text].fake += 1;
+        voteflag[msg.sender] = text;
+        voteCount[msg.sender] += 1;
+        if(voteCount[msg.sender] >= 5){
+            getIncentive();
+        }
+    }
+
+    
+    function voteTrue(uint8 text) public{
+        require(voteflag[msg.sender] != text);
+        votestatus[text].notfake += 1;
+        voteflag[msg.sender] = text;
+        voteCount[msg.sender] += 1;
+        if(voteCount[msg.sender] >= 5){
+            getIncentive();
+        }
+    }
+    
+    function getIncentive() private{
+        msg.sender.transfer(0.05 ether);
+    }
+    
+    function transferAmount() public payable{
+        amount = msg.value;
+    }
+    
+    function viewCount(uint8 text) public view returns(uint,uint){
+        require(amount == 0.1 ether);
+        return(votestatus[text].notfake, votestatus[text].fake);
+    }
+}
